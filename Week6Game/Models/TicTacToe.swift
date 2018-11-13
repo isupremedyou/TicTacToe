ba82010d26fb7ef2f31013e9543c759db6e68c1c
Travis <travis@travischapman.org>
2018-11-13 15:02:35 -0700

    adds models and model controller

--- Week6Game/Models/TicTacToe.swift
+++ Week6Game/Models/TicTacToe.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+class TicTacToe {
+    
+    /// Coordinates to the TicTacToe Gameboard Spaces where the letter refers to the Y-axis and the number refers to the X-axis
+    enum Point: String {
+        case a1, a2, a3, b1, b2, b3, c1, c2, c3
+    }
+    
+    enum GameOutcome: String {
+        case win, draw, keepPlaying
+    }
+}
+
+class Player {
+    
+    var name: String?
+    var points: [TicTacToe.Point]
+    
+    init(name: String?) {
+        
+        self.name = name
+        self.points = [TicTacToe.Point]()
+    }
+}
