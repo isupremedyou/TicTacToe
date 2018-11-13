7a0988dcd000d75261eb3bf569dfd05339271341
Travis Chapman <40254533+travischapman@users.noreply.github.com>
2018-11-13 15:00:34 -0700

    Revert "Travis"

--- Week6Game/ModelControllers/TicTacToeController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  TicTacToeController.swift
-//  Week6Game
-//
-//  Created by Travis Chapman on 11/13/18.
-//  Copyright © 2018 Travis Chapman. All rights reserved.
-//
-
-import Foundation
--- Week6Game/Models/TicTacToe.swift
+++
@@ -1,9 +0,0 @@
-//
-//  TicTacToe.swift
-//  Week6Game
-//
-//  Created by Travis Chapman on 11/13/18.
-//  Copyright © 2018 Travis Chapman. All rights reserved.
-//
-
-import Foundation
